605bc2b514a6187854de559211c54472ef2e2ce6
Todd Gore <github@toddgore.com>
2018-10-10 20:46:10 -0600

    Testing for in loop

--- js/code.js
+++ js/code.js
@@ -1,29 +1,13 @@
 let string = "Hello"
 
 
-function reverseString(str) {
-    let tempStr = ""
-    console.log('length ', str.length);
-
-    for (i = str.length - 1; i >= 0; i--) {
-        // What does i represent? An array of Characters!
-        // i starts at 0.
-        tempStr += str[i]
-    }
-    return tempStr
-}
-
-console.log(reverseString(string))
-
-
-
-function reverse2(str) {
+function reverse(str) {
     let reversed = ""
     for (let char of str) {
         reversed = char + reversed
     }
     return reversed
 }
-console.log('reverse2 ', reverse2(string));
+console.log('reverse ', reverse(string));
 
-
+// for each, for of, for each
